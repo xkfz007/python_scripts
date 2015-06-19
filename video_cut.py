eed6f24a30823fd3a1cb3982c9e76a0577ab2c4c
Fangzhen <xkfz007@users.noreply.github.com>
2015-06-19 11:11:33 +0800

    add operation 2 add yuvplayer.py

--- video_cut.py
+++ video_cut.py
@@ -19,6 +19,9 @@ audio_codecs={'mp3':codec_t('mp3','.mp3','libmp3lame'),
               'aac':codec_t('aac','.aac','libvo_aacenc'),
               'vorbis':codec_t('vorbis','.ogg','libvorbis')
               }
+video_ext_list=('.mkv','.avi','.rmvb','.rm','.mp4','.flv','.f4v','.wmv')
+audio_ext_list=('.mp3','.aac','.ogg','.wav')
+pic_ext_list=('.png','.jpg','.gif','.bmp','.tiff')
 
 def get_audio_codec(opt_list):
   cmd_line = "%s" % opt_list['ffmpeg.exe']
@@ -49,8 +52,8 @@ def get_audio_codec(opt_list):
 def usage():
   help_msg = '''Usage:./test.py [option] [value]...
   options:
-   -i <string> input_path
-   -o <string> output_path
+   -i <string> input file
+   -o <string> output file
    -s 00:00:00 start time offset
    -t <int>or<hh:mm:ss> duration be a number in seconds, or in hh:mm:ss[.xxx] form.
    -T <int>or<hh:mm:ss> duration be a number in seconds, or in hh:mm:ss[.xxx] form.
@@ -58,6 +61,8 @@ def usage():
    -H show the ffmpeg help
    -O 0: cut video
       1: get audio from video
+      2: convert the video to pictures
+   -e extension of output file
    -y overwrite the exist files
    -n do not overwrite the exist files
 
@@ -75,6 +80,7 @@ def get_default_opts():
   opt_list['position']='-1'
   opt_list['help']=0
   opt_list['operation']=-1
+  opt_list['extension']=''
   opt_list['overwrite']='u'
   return opt_list
 
@@ -84,7 +90,7 @@ def parse_cl(opt_list):
     sys.exit()
 
   try:
-    opts, args = getopt.getopt(sys.argv[1:], 'i:o:s:t:T:hHO:yn')
+    opts, args = getopt.getopt(sys.argv[1:], 'i:o:s:t:T:hHO:e:yn')
   except getopt.GetoptError as err:
     print str(err)
     sys.exit(2)
@@ -104,6 +110,8 @@ def parse_cl(opt_list):
       opt_list["position"] = arg
     elif opt == "-O":
       opt_list["operation"] = int(arg)
+    elif opt == "-e":
+      opt_list["extension"] = arg
     elif opt == "-y":
       opt_list["overwrite"] = 'y'
     elif opt == "-n":
@@ -116,8 +124,13 @@ def parse_cl(opt_list):
     else:
       assert False, "unknown option"
 
+
 def get_cmd_line(opt_list):
   cmd_line=""
+  #set the input file
+  cmd_line += " -i %s" % opt_list['input_file']
+
+  #set the start delay and duration
   if opt_list['start_time']!='-1':
     cmd_line += " -ss %s" % opt_list['start_time']
   if opt_list['duration']!='-1':
@@ -125,24 +138,39 @@ def get_cmd_line(opt_list):
   if opt_list['duration']=='-1' and opt_list['position']!='-1':
     cmd_line += " -T %s" % opt_list['position']
 
+  #set the output file
+  file_name,ext=os.path.splitext(opt_list['input_file'])
   if opt_list['operation']==0:#video cut
     if opt_list['output_file']=='':
-      file_name,ext=os.path.splitext(opt_list['input_file'])
+      if opt_list['extension']!='' and opt_list['extension'] in video_ext_list:
+        ext=opt_list['extension']
       opt_list['output_file']=file_name+"_out"+ext
-    cmd_line += " -i %s -vcodec copy -acodec copy" % opt_list['input_file']
-    cmd_line += " %s" % opt_list['output_file']
+    #cmd_line += " -i %s -vcodec copy -acodec copy" % opt_list['input_file']
+    cmd_line += " -vcodec copy"
+    cmd_line += " -acodec copy"
+    #cmd_line += " %s" % opt_list['output_file']
   elif opt_list['operation']==1:#get audio from video
     if opt_list['output_file']=='':
-      file_name,ext=os.path.splitext(opt_list['input_file'])
+      #file_name,ext=os.path.splitext(opt_list['input_file'])
       ac=get_audio_codec(opt_list)
       ext=audio_codecs[ac].ext
+      if opt_list['extension']!='' and opt_list['extension'] in video_ext_list:
+        ext=opt_list['extension']
       opt_list['output_file']=file_name+"_audio"+ext
-    cmd_line += " -i %s" % opt_list['input_file']
+    #cmd_line += " -i %s" % opt_list['input_file']
     cmd_line += " -vn"#disable video output
     cmd_line += " -acodec copy"
     #cmd_line += " -acodec %s"% audio_codecs[ac].codec
-    cmd_line += " %s" % opt_list['output_file']
+    #cmd_line += " %s" % opt_list['output_file']
+  elif opt_list['operation']==2:
+    if opt_list['output_file']=='':
+      if opt_list['extension']!='' and opt_list['extension'] in video_ext_list:
+        ext=opt_list['extension']
+      else:
+        ext='.png'
+      opt_list['output_file']=file_name+"%d"+ext
 
+  cmd_line += " %s" % opt_list['output_file']
   return cmd_line
 
 def get_file_list(path_dir):
---
+++ yuvplayer.py
@@ -0,0 +1,41 @@
+#!/bin/python
+import getopt
+import sys
+import subprocess
+import lib.seq_list
+import lib.fun_lib
+import os
+__author__ = 'hfz2597'
+
+#opts, args = getopt.getopt(sys.argv[1:], 'i:o:s:t:T:hHO:e:yn')
+#print opts
+#print args
+executor="ffplay.exe "
+location=""
+cmd_list=[]
+for i in sys.argv[1:]:
+  name=os.path.basename(i)
+  dpath=os.path.dirname(i)
+  if location!=dpath and dpath!="":
+    location=dpath
+  if not name.endswith(".yuv"):
+    name=lib.seq_list.get_seqname(name)
+    name+=".yuv"
+  reso=name.split('_')[1]
+  cmd=executor
+  full_path=os.path.join(location,name)
+  cmd+=" -i %s"%full_path
+  cmd+=" -f rawvideo"
+  cmd+=" -video_size %s"%reso
+  cmd_list.append(cmd)
+
+print cmd_list
+
+for i in cmd_list:
+  subprocess.Popen(i,stdout=None,stderr=None,shell=True)
+
+
+
+#cmd_line="ffplay.exe -i f:/sequences/Dblowingbubbles_416x240_8_50_500.yuv  -f rawvideo -video_size 416x240"
+#subprocess.Popen(cmd_line,stdout=None,stderr=None,shell=True)
+#subprocess.Popen(cmd_line,stdout=None,stderr=None,shell=True)
