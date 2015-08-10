f9e9b9a0f67d57faa282c903a2694aebe5a276ee
Fangzhen <xkfz007@users.noreply.github.com>
2015-08-10 15:40:21 +0800

    use extension to decide the operation

--- pyff.py
+++ pyff.py
@@ -17,6 +17,14 @@ h264_name_list = ('h264', 'h.264', 'avc', '264')
 h265_name_list = ('h265', 'h.265', 'hevc', '265')
 bsf_for_mp4 = {'h264': 'h264_mp4toannexb',
                'hevc': 'hevc_mp4toannexb'}
+bitstream_name_list=h264_name_list+h265_name_list
+yuv_name_list=('yuv','y4m')
+mp4_name_list=('mp4','flv','mkv','f4v')
+avi_name_list=('avi',)
+video_name_list=mp4_name_list+avi_name_list
+audio_name_list=('aac','mp3','ogg')
+
+valid_ext_list=h264_name_list+h265_name_list+yuv_name_list+video_name_list+audio_name_list
 
 
 def usage():
@@ -24,10 +32,13 @@ def usage():
   options:
    -i <string> input file, support pattern globbing
    -e <string> the output file extension
+      h264/hevc: get the raw bitstream from media file
+      yuv: decode the input to get rawvideo(yuv)
+      aac,mp3: extract audio from media file
+   -a extract audio using the extra command lines from -C,
+      ignore the auto generated command lines from extension(-e)
    -C <string> the ffmpeg commands
    -t <int> set thread num
-   -d decode the input to get rawvideo(yuv)
-   -b <streamtype:h264,hevc> get the raw bitstream from media file
    -w <int> set the output width
    -h <int> set the output height
    -Y whether to execute the program
@@ -37,22 +48,29 @@ def usage():
     return
 
 
-def get_cmd_line(input_file, ext, output_tmp, prepared_cmd, extra_cmd, bitstream_type):
+def get_cmd_line(input_file, ext, output_tmp, prepared_cmd, extra_cmd):
     fname, fext = os.path.splitext(input_file)
     print 'fname="%s"' % fname
     print 'fext="%s"' % fext
-    if len(ext) > 0:  # '':
-        if not ext.startswith('.'):
-            ext = '.' + ext
-    else:
-        ext = fext
-    output_file = "%s%s%s" % (fname, output_tmp, ext)
+    fext=fext.lower()[1:]
+
+    if len(ext) == 0 :
+        if len(fext)>0:
+            ext = fext
+        else:#set default ext
+            ext='ext'
+    elif len(ext) > 0:
+        if ext in bitstream_name_list and fext =='mp4':
+            extra_cmd += " -bsf %s" % bsf_for_mp4[ext]
+        #elif ext in audio_name_list:
+        #    if fext in mp4_name_list:
+        #        extra_cmd+=" -c:a copy"
+
+    output_file = "%s%s.%s" % (fname, output_tmp, ext)
 
     cmd_line = prepared_cmd
     cmd_line += " -i \"%s\"" % input_file
     cmd_line += " %s" % extra_cmd
-    if len(bitstream_type) > 1 and fext in (".mp4", ".MP4"):
-        cmd_line += " -bsf %s" % bsf_for_mp4[bitstream_type]
     cmd_line += " \"%s\"" % output_file
     return cmd_line
 
@@ -65,6 +83,14 @@ def get_default_opt_list():
     # opt_list['output_tmp'] = ''
     opt_list['thread_num'] = 0
     opt_list['do_execute'] = 0
+    return opt_list
+
+def get_ffmpeg_opt_list():
+    ff_opt_list={}
+    ff_opt_list['input_file']=''
+    ff_opt_list['vcodec']=''
+    ff_opt_list['acodec']=''
+    return ff_opt_list
 
 
 if __name__ == '__main__':
@@ -73,7 +99,7 @@ if __name__ == '__main__':
         sys.exit()
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'i:e:C:t:db:w:h:YH')
+        opts, args = getopt.getopt(sys.argv[1:], 'i:e:aC:t:w:h:YH')
     except getopt.GetoptError as err:
         print str(err)
         sys.exit(2)
@@ -84,11 +110,12 @@ if __name__ == '__main__':
     input_tmp = ''
     output_tmp = ''
     extra_cmd = ''
-    ext = ''
+    extension = ''
     thread_num = 0
     do_execute = 0
-    decode_flag = 0
-    bitstream_type = ""
+    #decode_flag = 0
+    #bitstream_type = ""
+    auto_audio_flag=1
     width=-2
     height=-2
     print opts
@@ -98,15 +125,17 @@ if __name__ == '__main__':
         if opt == "-i":
             input_tmp = arg
         elif opt == "-e":
-            ext = arg
+            extension = arg
+        elif opt == "-a":
+            auto_audio_flag= 0
         elif opt == "-C":
             extra_cmd = arg
         elif opt == "-t":
             thread_num = int(arg)
-        elif opt == "-d":
-            decode_flag = 1
-        elif opt == "-b":
-            bitstream_type = arg
+        #elif opt == "-d":
+        #    decode_flag = 1
+        #elif opt == "-b":
+        #    bitstream_type = arg
         elif opt == "-w":
             width= int(arg)
         elif opt == "-h":
@@ -136,8 +165,8 @@ if __name__ == '__main__':
         print '"%s" is glob pattern' % input_tmp
         input_list = glob.glob(input_tmp)
     # print type(input_file)
-    print "input_list=%s" % input_list
-    if len(input_list) == 0:  # '':# or ( ext=='' and output_file==''):
+    #print "input_list=%s" % input_list
+    if len(input_list) == 0:
         usage()
         sys.exit()
 
@@ -158,22 +187,33 @@ if __name__ == '__main__':
     if thread_num > 0:
         prepared_cmd += " -threads %s" % thread_num
 
-    if decode_flag == 1:
-        ext = '.yuv'
-        output_tmp = '_rec'
-        extra_cmd += " -an -f rawvideo"
-    elif len(bitstream_type) > 1:
-        if bitstream_type.startswith('.'):
-            bitstream_type = bitstream_type[1:]
+    if len(extension)>0 :
+        if extension.startswith('.'):
+           extension =  extension[1:]
+        extension=extension.lower()
+        if not extension in valid_ext_list:
+            print "Error: invaild extension, please check"
+            sys.exit()
 
-        if bitstream_type.lower() in h264_name_list:
-            bitstream_type = 'h264'
-        elif bitstream_type.lower() in h265_name_list:
-            bitstream_type = 'hevc'
-        ext = '.' + bitstream_type
+        if extension in yuv_name_list:# decode to get raw yuv
+            output_tmp = '_rec'
+            extra_cmd += " -an -f rawvideo"
+        elif extension in bitstream_name_list:# just get the raw stream
+            output_tmp= '_bin'
+            if extension in h264_name_list:
+                extension= 'h264'
+            elif extension in h265_name_list:
+                extension= 'hevc'
+            extra_cmd += " -c:v copy"
+            extra_cmd += " -an -f %s" % extension
+        elif extension in audio_name_list:
+            output_tmp='_aud'
+            extra_cmd+=" -vn"
+            if auto_audio_flag==1:
+                extra_cmd+=" -c:a copy"
+            else:
+                extra_cmd+=" -b:a 64k"#set bitrate for audio
 
-        extra_cmd += " -c:v copy"
-        extra_cmd += " -an -f %s" % bitstream_type
     elif width>0 or height >0:
         extra_cmd += " -c:a copy"
         extra_cmd+=" -vf scale=%s:%s"%(width,height)
@@ -183,7 +223,7 @@ if __name__ == '__main__':
         print input_file
         if os.path.isfile(input_file):
             print '"%s" is a file' % input_file
-            cmd_line = get_cmd_line(input_file, ext, output_tmp, prepared_cmd, extra_cmd, bitstream_type)
+            cmd_line = get_cmd_line(input_file, extension, output_tmp, prepared_cmd, extra_cmd)
             print cmd_line
             cmd_list.append(cmd_line)
         elif os.path.isdir(input_file):
@@ -191,12 +231,13 @@ if __name__ == '__main__':
             input_file_list2 = lib.get_file_list(input_file)
             print "input_file_list2=%s" % input_file_list2
             for input_file2 in input_file_list2:
-                cmd_line = get_cmd_line(input_file2, ext, output_tmp, prepared_cmd, extra_cmd, bitstream_type)
+                cmd_line = get_cmd_line(input_file2, extension, output_tmp, prepared_cmd, extra_cmd)
                 print cmd_line
                 cmd_list.append(cmd_line)
 
     if do_execute == 1:
         for cmd in cmd_list:
+            print cmd
             # os.system(cmd)
             subprocess.call(cmd, stdout=None, stderr=None, shell=True)
 
--- test/test.py
+++ test/test.py
@@ -22,117 +22,124 @@ import subprocess
 #print idx_list
 
 
-print platform.architecture()
-print platform.platform()
-print platform.system()
-print platform.python_version()
-print platform.uname()
-print sys.platform
-#os.system("pause")
-str = "cygwin\\"
-print str
-a = str.replace(":", "")
-print str
-print a
-
-print sys.argv
-
-str = "hello"
-print str
-str += "world"
-print str
-
-
-def update_str(str, str2):
-    str += str2
-    return str
-
-
-print update_str(str, "OK")
-
-a = "-12.7"
-print a
-b = float(a)
-print b
-
-#tmp_dict={'a':1,'b':2,'c':3}
-#if tmp_dict.has_key('d') and tmp_dict['d']==4:
-#  print 'OK'
-#elif tmp_dict['d']==4:
-#  print 'invalid'
-#else:
-#  print 'BAD'
+# print platform.architecture()
+# print platform.platform()
+# print platform.system()
+# print platform.python_version()
+# print platform.uname()
+# print sys.platform
+# #os.system("pause")
+# str = "cygwin\\"
+# print str
+# a = str.replace(":", "")
+# print str
+# print a
 #
-
-#def get_total_frame_num(filename,width,height):
-#  size=os.path.getsize(filename)
-#  filesize=width*height*3/2
-#  num_frames=size/filesize
-#  return num_frames
+# print sys.argv
 #
-#print "num_frames=%s" % get_total_frame_num("e:/sequences/navyflight_1920x1080_30_2973.yuv",1920,1080)
+# str = "hello"
+# print str
+# str += "world"
+# print str
 #
-#a=100
-#for a in range(10):
-#  print a
 #
-#print a
+# def update_str(str, str2):
+#     str += str2
+#     return str
 #
-#path="c:/aa/kk"
-#for i in [ "  c://a//kk","c:\\dfa\\asf\\   ","c://adfa\\adsf   \\","c:/fasdf\\fasdf/  "]:
-#  print "%s@@@@%s"%(i,os.path.normpath(i))
 #
-#path="c:/aa/kkk"
-#path=os.path.normpath(path)
-#filename="a.txt"
-#fullname=os.path.join(path,filename)
-#print fullname
-
-
-#path="F:\\tmp"
-#for root,dirs,files in os.walk(path):
-#  #print "[root=%s dirs=%s files=%s]"%(root,dirs,files)
-#  for i in files:
-#    print "FILE=%s"%os.path.join(root,i)
-
-#import lib.fun_lib
-#seq_name=( 'Apeopleonstreet_2560x1600_8_30_150','Atraffic_2560x1600_8_30_150','Bbasketballdrive_1920x1080_8_50_500',
-#'Bbqterrace_1920x1080_8_60_600','Bcactus_1920x1080_8_50_500','Bkimono_1920x1080_8_24_240','RaceHorses_416x240_30',
-#'RaceHorses_832x480_30','SlideEditing_1280x720_30','SlideShow_1280x720_20','Tennis_1920x1080_24',
-#'Traffic_2560x1600_30_crop','vidyo1_1280x720_60','vidyo3_1280x720_60','vidyo4_1280x720_60','walk_640x480_30',
-#'xtslf_1920x1080_25_1854',  )
-#for i in seq_name:
-#  width,height,fps=lib.fun_lib.get_reso_info(i)
-#  print "%s:%d %d %d"%(i,width,height,fps)
-
-#import subprocess
-#p=subprocess.Popen('ls')
-#print p.stdout
-
-path = "F:\\encoder_test_output\\x265_output"
-#print os.path.abspath(path)
-print os.path.normpath(path)
-#print os.path.normpath(os.path.abspath(path))
-#print os.path.relpath(".")
-#print os.path.splitdrive(path)
-print os.sep
+# print update_str(str, "OK")
 #
-#dictString = "{'Define1':[[63.3,0.00,0.5,0.3,0.0],[269.3,0.034,1.0,1.0,0.5],[332.2,0.933,0.2,0.99920654296875,1],[935.0,0.990,0.2,0.1,1.0]],'Define2':[[63.3,0.00,0.5,0.2,1.0],[269.3,0.034,1.0,0.3,0.5],[332.2,0.933,0.2, 0.4,0.6],[935.0,0.990,1.0, 0.5,0.0]],}"
-#dict = eval(dictString)
-#print "keys:", dict.keys()
-#print "Define1 value ", dict['Define1']
+# a = "-12.7"
+# print a
+# b = float(a)
+# print b
+#
+# #tmp_dict={'a':1,'b':2,'c':3}
+# #if tmp_dict.has_key('d') and tmp_dict['d']==4:
+# #  print 'OK'
+# #elif tmp_dict['d']==4:
+# #  print 'invalid'
+# #else:
+# #  print 'BAD'
+# #
+#
+# #def get_total_frame_num(filename,width,height):
+# #  size=os.path.getsize(filename)
+# #  filesize=width*height*3/2
+# #  num_frames=size/filesize
+# #  return num_frames
+# #
+# #print "num_frames=%s" % get_total_frame_num("e:/sequences/navyflight_1920x1080_30_2973.yuv",1920,1080)
+# #
+# #a=100
+# #for a in range(10):
+# #  print a
+# #
+# #print a
+# #
+# #path="c:/aa/kk"
+# #for i in [ "  c://a//kk","c:\\dfa\\asf\\   ","c://adfa\\adsf   \\","c:/fasdf\\fasdf/  "]:
+# #  print "%s@@@@%s"%(i,os.path.normpath(i))
+# #
+# #path="c:/aa/kkk"
+# #path=os.path.normpath(path)
+# #filename="a.txt"
+# #fullname=os.path.join(path,filename)
+# #print fullname
+#
+#
+# #path="F:\\tmp"
+# #for root,dirs,files in os.walk(path):
+# #  #print "[root=%s dirs=%s files=%s]"%(root,dirs,files)
+# #  for i in files:
+# #    print "FILE=%s"%os.path.join(root,i)
+#
+# #import lib.fun_lib
+# #seq_name=( 'Apeopleonstreet_2560x1600_8_30_150','Atraffic_2560x1600_8_30_150','Bbasketballdrive_1920x1080_8_50_500',
+# #'Bbqterrace_1920x1080_8_60_600','Bcactus_1920x1080_8_50_500','Bkimono_1920x1080_8_24_240','RaceHorses_416x240_30',
+# #'RaceHorses_832x480_30','SlideEditing_1280x720_30','SlideShow_1280x720_20','Tennis_1920x1080_24',
+# #'Traffic_2560x1600_30_crop','vidyo1_1280x720_60','vidyo3_1280x720_60','vidyo4_1280x720_60','walk_640x480_30',
+# #'xtslf_1920x1080_25_1854',  )
+# #for i in seq_name:
+# #  width,height,fps=lib.fun_lib.get_reso_info(i)
+# #  print "%s:%d %d %d"%(i,width,height,fps)
+#
+# #import subprocess
+# #p=subprocess.Popen('ls')
+# #print p.stdout
+#
+# path = "F:\\encoder_test_output\\x265_output"
+# #print os.path.abspath(path)
+# print os.path.normpath(path)
+# #print os.path.normpath(os.path.abspath(path))
+# #print os.path.relpath(".")
+# #print os.path.splitdrive(path)
+# print os.sep
+# #
+# #dictString = "{'Define1':[[63.3,0.00,0.5,0.3,0.0],[269.3,0.034,1.0,1.0,0.5],[332.2,0.933,0.2,0.99920654296875,1],[935.0,0.990,0.2,0.1,1.0]],'Define2':[[63.3,0.00,0.5,0.2,1.0],[269.3,0.034,1.0,0.3,0.5],[332.2,0.933,0.2, 0.4,0.6],[935.0,0.990,1.0, 0.5,0.0]],}"
+# #dict = eval(dictString)
+# #print "keys:", dict.keys()
+# #print "Define1 value ", dict['Define1']
+#
+# print os.getcwd()
+# print os.curdir
+# print os.pardir
+# print os.path.abspath('\\\\172.21.40.64\\h265\\xml')
+#
+# import lib
+#
+# a = lib.get_total_frame_num("f:/sequences/kldby_1920x1080_25.yuv", 200, 200)
+# print a
+# print __file__
+# print os.path.realpath(__file__)
 
-print os.getcwd()
-print os.curdir
-print os.pardir
-print os.path.abspath('\\\\172.21.40.64\\h265\\xml')
+def str_func(str,i):
+    str+="test%s"%i
+    print str
 
-import lib
-
-a = lib.get_total_frame_num("f:/sequences/kldby_1920x1080_25.yuv", 200, 200)
-print a
-print __file__
-print os.path.realpath(__file__)
-
+str="good"
+for i in range(5):
+    str_func(str,i)
 
 
