4a151a878a00434549dd161da813c15888ce89ca
Fangzhen <xkfz007@users.noreply.github.com>
2015-08-10 18:20:58 +0800

    support file merge

--- pyff.py
+++ pyff.py
@@ -28,21 +28,25 @@ valid_ext_list=h264_name_list+h265_name_list+yuv_name_list+video_name_list+audio
 
 
 def usage():
-    help_msg = '''Usage:pyff.py [option] [value]...
+    help_msg = '''Usage:pyff.py [option] <input1> <input2> ...
   options:
-   -i <string> input file, support pattern globbing
    -e <string> the output file extension
       h264/hevc: get the raw bitstream from media file
       yuv: decode the input to get rawvideo(yuv)
       aac,mp3: extract audio from media file
    -a extract audio using the extra command lines from -C,
       ignore the auto generated command lines from extension(-e)
+   -o <string> output file tag
    -C <string> the ffmpeg commands
    -t <int> set thread num
    -w <int> set the output width
    -h <int> set the output height
+   -m <string> merge the input media files to one
    -Y whether to execute the program
    -H print the FFMpeg help
+   arguments:
+   <input1> <input2> <input3> file or directory, support pattern globbing
+           these are the input files that will be processed
    '''
     print help_msg
     return
@@ -61,10 +65,18 @@ def get_cmd_line(input_file, ext, output_tmp, prepared_cmd, extra_cmd):
             ext='ext'
     elif len(ext) > 0:
         if ext in bitstream_name_list and fext =='mp4':
-            extra_cmd += " -bsf %s" % bsf_for_mp4[ext]
+            extra_cmd += " -bsf:v %s" % bsf_for_mp4[ext]
         #elif ext in audio_name_list:
         #    if fext in mp4_name_list:
         #        extra_cmd+=" -c:a copy"
+        if ext=='TS':
+            if fext in mp4_name_list:
+                extra_cmd+=" -c copy"
+                if fext=='mp4':
+                   extra_cmd += " -bsf:v %s" % bsf_for_mp4['h264']
+            else:
+                extra_cmd+=" -c:v libx264 -c:a libvo_aacenc"
+
 
     output_file = "%s%s.%s" % (fname, output_tmp, ext)
 
@@ -72,7 +84,7 @@ def get_cmd_line(input_file, ext, output_tmp, prepared_cmd, extra_cmd):
     cmd_line += " -i \"%s\"" % input_file
     cmd_line += " %s" % extra_cmd
     cmd_line += " \"%s\"" % output_file
-    return cmd_line
+    return (cmd_line,output_file)
 
 
 def get_default_opt_list():
@@ -92,6 +104,26 @@ def get_ffmpeg_opt_list():
     ff_opt_list['acodec']=''
     return ff_opt_list
 
+def get_input_list(input_list,input_tmp):
+    print "input_tmp=%s" % input_tmp
+    #if len(input_tmp) == 0:
+    #    usage()
+
+    if os.path.isdir(input_tmp):
+        print '"%s" is a directory' % input_tmp
+        input_list.extend(lib.get_file_list(input_tmp))
+    elif os.path.isfile(input_tmp):
+        print '"%s" is a file' % input_tmp
+        #input_list = []
+        input_list.append(input_tmp)
+    else:  # globbing is needed
+        print '"%s" is glob pattern' % input_tmp
+        glob_list=glob.glob(input_tmp)
+        for i in glob_list:
+            get_input_list(input_list,i)
+    # print type(input_file)
+    #print "input_list=%s" % input_list
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
@@ -99,7 +131,7 @@ if __name__ == '__main__':
         sys.exit()
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'i:e:aC:t:w:h:YH')
+        opts, args = getopt.gnu_getopt(sys.argv[1:], 'o:e:aC:t:w:h:m:YH')
     except getopt.GetoptError as err:
         print str(err)
         sys.exit(2)
@@ -118,12 +150,13 @@ if __name__ == '__main__':
     auto_audio_flag=1
     width=-2
     height=-2
+    merged_file=''
     print opts
     print args
 
     for opt, arg in opts:
-        if opt == "-i":
-            input_tmp = arg
+        if opt == "-o":
+            output_tmp = arg
         elif opt == "-e":
             extension = arg
         elif opt == "-a":
@@ -132,14 +165,12 @@ if __name__ == '__main__':
             extra_cmd = arg
         elif opt == "-t":
             thread_num = int(arg)
-        #elif opt == "-d":
-        #    decode_flag = 1
-        #elif opt == "-b":
-        #    bitstream_type = arg
         elif opt == "-w":
             width= int(arg)
         elif opt == "-h":
             height= int(arg)
+        elif opt=="-m":
+            merged_file=arg
         elif opt == "-Y":
             do_execute = 1
         elif opt == "-H":
@@ -149,33 +180,17 @@ if __name__ == '__main__':
         else:
             assert False, "unknown option"
 
-    print "input_tmp=%s" % input_tmp
-    if len(input_tmp) == 0:
-        usage()
+    if len(args) == 0:
+        print "Error: No input is specified, please check"
         sys.exit()
+    input_list=[]
+    for arg in args:
+        get_input_list(input_list,arg)
 
-    if os.path.isdir(input_tmp):
-        print '"%s" is a directory' % input_tmp
-        input_list = lib.get_file_list(input_tmp)
-    elif os.path.isfile(input_tmp):
-        print '"%s" is a file' % input_tmp
-        input_list = []
-        input_list.append(input_tmp)
-    else:  # globbing is needed
-        print '"%s" is glob pattern' % input_tmp
-        input_list = glob.glob(input_tmp)
-    # print type(input_file)
-    #print "input_list=%s" % input_list
     if len(input_list) == 0:
-        usage()
+        print "Error: Input is invalid, please check"
         sys.exit()
 
-    if len(args) > 0:  # '':
-        #if len(args) == 1:
-        #    output_tmp = args
-        #else:
-        #    output_tmp = args[0]
-        output_tmp=args[0]
     if len(output_tmp) == 0:  # =='':
         output_tmp = "_out"
     else:
@@ -187,7 +202,9 @@ if __name__ == '__main__':
     if thread_num > 0:
         prepared_cmd += " -threads %s" % thread_num
 
-    if len(extension)>0 :
+    if len(merged_file)>0:
+        extension='TS'
+    elif len(extension)>0 :
         if extension.startswith('.'):
            extension =  extension[1:]
         extension=extension.lower()
@@ -218,28 +235,44 @@ if __name__ == '__main__':
         extra_cmd += " -c:a copy"
         extra_cmd+=" -vf scale=%s:%s"%(width,height)
 
+
     cmd_list = []
+    output_list=[]
     for input_file in input_list:
         print input_file
-        if os.path.isfile(input_file):
-            print '"%s" is a file' % input_file
-            cmd_line = get_cmd_line(input_file, extension, output_tmp, prepared_cmd, extra_cmd)
-            print cmd_line
-            cmd_list.append(cmd_line)
-        elif os.path.isdir(input_file):
-            print '"%s" is a directory' % input_file
-            input_file_list2 = lib.get_file_list(input_file)
-            print "input_file_list2=%s" % input_file_list2
-            for input_file2 in input_file_list2:
-                cmd_line = get_cmd_line(input_file2, extension, output_tmp, prepared_cmd, extra_cmd)
-                print cmd_line
-                cmd_list.append(cmd_line)
+        if not os.path.isfile(input_file):
+            print 'Error: "%s" is not file, but is in the input_list'%input_file
+        cmd_line,output_file = get_cmd_line(input_file, extension, output_tmp, prepared_cmd, extra_cmd)
+        print cmd_line
+        cmd_list.append(cmd_line)
+        output_list.append(output_file)
+        #elif os.path.isdir(input_file):
+        #    print '"%s" is a directory' % input_file
+        #    input_file_list2 = lib.get_file_list(input_file)
+        #    print "input_file_list2=%s" % input_file_list2
+        #    for input_file2 in input_file_list2:
+        #        cmd_line = get_cmd_line(input_file2, extension, output_tmp, prepared_cmd, extra_cmd)
+        #        print cmd_line
+        #        cmd_list.append(cmd_line)
+    if len(merged_file)>0:
+        concat_str = "|".join(output_list)
+        print concat_str
+        cmd_line = FFMPEG_BIN
+        cmd_line += ' -i "concat:%s" -c copy -bsf:a aac_adtstoasc %s' % (concat_str, merged_file)
+        print cmd_line
+        cmd_list.append(cmd_line)
+
 
     if do_execute == 1:
         for cmd in cmd_list:
             print cmd
             # os.system(cmd)
             subprocess.call(cmd, stdout=None, stderr=None, shell=True)
+        if len(merged_file)>0:
+            for i in output_list:
+                if os.path.exists(i):
+                    os.remove(i)
+                    print '"%s" is deleted'%i
 
 
 
