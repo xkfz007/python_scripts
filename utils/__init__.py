8d82386d2e308a5b38a095aa21f6aab1e62b5b8a
Fangzhen <xkfz007@users.noreply.github.com>
2016-02-18 22:25:19 +0800

    add init file

---
+++ utils/__init__.py
@@ -0,0 +1,12 @@
+# import fun_lib
+# import as265_cmd_init
+#import global_var
+#import encoder_cmd
+#import common_lib
+#from fun_lib import *
+#from encoder_global_var import *
+from common_lib import *
+from TaskManager import *
+from log import *
+from help import *
+
